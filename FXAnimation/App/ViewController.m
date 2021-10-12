ca1c776cbc140811e0a22e744cb8e608eccd38e7
冯保奎 <fengbaokui@cnstrong.cn>
2021-10-12 18:21:56 +0800

    ...

--- FXAnimation/App/ViewController.m
+++ FXAnimation/App/ViewController.m
@@ -30,33 +30,47 @@
 #pragma mark - Data
 - (void)setupData {
     self.dataArray = @[
-    @{@"title": @"平移+旋转+变色", @"vc": @"CABasicAnimationVC"},
-    @{@"title": @"layer展示图片", @"vc": @"CGImageVC"},
-    @{@"title": @"垂直轴偏移", @"vc": @"ZPositionVC"},
-    @{@"title": @"事件穿透", @"vc": @"HitTestVC"},
-    @{@"title": @"子视图超出父视图范围", @"vc": @"FXTabBarController"},
-    @{@"title": @"旋转", @"vc": @"CGTransform3DVC"},
-    @{@"title": @"旋转综合训练", @"vc": @"CGTransformPracticeVC"},
-    @{@"title": @"旋转图层", @"vc": @"CATransformLayerVC"},
-    @{@"title": @"渐变色图层", @"vc": @"CAGradientLayerVC"},
-    @{@"title": @"反射图层", @"vc": @"CAReplicatorLayerVC"},
-    @{@"title": @"粒子发射", @"vc": @"CAEmitterLayerVC"},
-    @{@"title": @"雨滴", @"vc": @"RainEmitterVC"},
-    @{@"title": @"点赞-动效", @"vc": @"ThumbVC"},
-    @{@"title": @"视频播放", @"vc": @"AVPlayLayerVC"},
-    @{@"title": @"摇晃抖动", @"vc": @"SharkVC"},
-    @{@"title": @"贝塞尔曲线路径", @"vc": @"BezierPathAnimationVC"},
-    @{@"title": @"拖拽小红点", @"vc": @"PanGestureVC"},
-    @{@"title": @"转场动画", @"vc": @"PushStyleVC"},
-    @{@"title": @"自定义转场动画", @"vc": @"CustomPushVC"},
-    @{@"title": @"喜欢-动效", @"vc": @"LikeVC"},
-    @{@"title": @"文本放大动效", @"vc": @"LabelVC"},
-    @{@"title": @"音乐播放效果", @"vc": @"MusicVC"},
-    @{@"title": @"持续放大缩小效果", @"vc": @"ScaleVC"},
+        @{@"title": @"基础动画", @"list": @[
+            @{@"title": @"平移+旋转+变色", @"vc": @"CABasicAnimationVC"},
+            @{@"title": @"layer展示图片", @"vc": @"CGImageVC"},
+            @{@"title": @"垂直轴偏移", @"vc": @"ZPositionVC"},
+            @{@"title": @"事件穿透", @"vc": @"HitTestVC"},
+        ]},
+        @{@"title": @"Layer图层", @"list": @[
+            
+        ]},
+        @{@"title": @"TabBar", @"list": @[
+            @{@"title": @"子视图超出父视图范围", @"vc": @"FXTabBarController"},
+        ]},
+        @{@"title": @"组合动画", @"list": @[
+            
+        ]},
+//    @{@"title": @"旋转", @"vc": @"CGTransform3DVC"},
+//    @{@"title": @"旋转综合训练", @"vc": @"CGTransformPracticeVC"},
+//    @{@"title": @"旋转图层", @"vc": @"CATransformLayerVC"},
+//    @{@"title": @"渐变色图层", @"vc": @"CAGradientLayerVC"},
+//    @{@"title": @"反射图层", @"vc": @"CAReplicatorLayerVC"},
+//    @{@"title": @"粒子发射", @"vc": @"CAEmitterLayerVC"},
+//    @{@"title": @"雨滴", @"vc": @"RainEmitterVC"},
+//    @{@"title": @"点赞-动效", @"vc": @"ThumbVC"},
+//    @{@"title": @"视频播放", @"vc": @"AVPlayLayerVC"},
+//    @{@"title": @"摇晃抖动", @"vc": @"SharkVC"},
+//    @{@"title": @"贝塞尔曲线路径", @"vc": @"BezierPathAnimationVC"},
+//    @{@"title": @"拖拽小红点", @"vc": @"PanGestureVC"},
+//    @{@"title": @"转场动画", @"vc": @"PushStyleVC"},
+//    @{@"title": @"自定义转场动画", @"vc": @"CustomPushVC"},
+//    @{@"title": @"喜欢-动效", @"vc": @"LikeVC"},
+//    @{@"title": @"文本放大动效", @"vc": @"LabelVC"},
+//    @{@"title": @"音乐播放效果", @"vc": @"MusicVC"},
+//    @{@"title": @"持续放大缩小效果", @"vc": @"ScaleVC"},
     ];
 }
 
 #pragma mark - UITableViewDataSource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return self.dataArray.count;
+}
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return self.dataArray.count;
 }
@@ -82,12 +96,13 @@
 #pragma mark - Lazy
 - (UITableView *)tableView {
     if (!_tableView) {
-        _tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+        _tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStyleGrouped];
         _tableView.showsHorizontalScrollIndicator = false;
         _tableView.showsVerticalScrollIndicator = false;
         _tableView.rowHeight = 52;
         _tableView.dataSource = self;
         _tableView.delegate = self;
+        _tableView.tableHeaderView = [UIView new];
         [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"Cell"];
     }
     return _tableView;
